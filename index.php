bed1a81444fcb584b4c05cd16fc185d4ca182aad
Nick Schappler <nickschappler@gmail.com>
2012-05-09 02:38:12 -0400

    image positioning styles to place glasses on face

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
     </head>
     
     <body>
-        Testing in this new branch
-        <?php
-            $dir = "./frames/";
-            if (is_dir($dir)){
-                if ($handle = opendir($dir)) {
-                    while (($file = readdir($handle)) !== false) {
-                        if ($file != "." and $file != ".."){
-                            $file_path = $dir . $file;
-                            echo "<img src=\"$file_path\" />";
-                        }
-                    }
-                }
-            }
-        
-        
-        ?>
+	
+	<div style="position: relative; top: 0; left: 0;">
+		<img src="./faces/face1.jpg" style="position: relative; top: 0; left: 0;">
+        	<?php
+            	$dir = "./frames/";
+            	if (is_dir($dir)){
+                	if ($handle = opendir($dir)) {
+                	    	while (($file = readdir($handle)) !== false) {
+                        		if ($file != "." and $file != ".."){
+                            			$file_path = $dir . $file;
+                            			echo "<img src=\"$file_path\" style=\"position: absolute; top: 0; left: 0;\"/>";
+                        		}
+                    		}	
+                	}
+           	 }
+     		?>
     </body>
 </html>
